7f27246774ad53bd554731c54f944c0cb5138c0d
Chetan Bura <chetan.bura@lntinfotech.com>
2022-09-26 12:48:34 +0530

    Jest configuration

--- src/ui/core/loader/loader.spec.tsx
+++ src/ui/core/loader/loader.spec.tsx
@@ -4,8 +4,7 @@ import { Loader } from './loader';
 
 describe('Loader', () => {
   it('should render successfully', async () => {
-    // Placeholder test designed to fail, so you look at me :)
-    render(<Loader />);
-    expect(screen.getByText('Does Not Exist')).toBeVisible();
+    render(<Loader text="Test" />);
+    expect(screen.getByText('Test')).toBeInTheDocument();
   });
 });
